c2275ac1ca168155348a69877ec7c1a3589e9096
Fitri Lestari <549.fitrilestari@gmail.com>
2020-07-04 13:25:11 -0700

    Tambah View Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fitri Lestari
                 </div>
 
                 <div class="links">
