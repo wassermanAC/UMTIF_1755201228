4aec8968e02e059cce3c0b3dbfada28de5e5e9d0
Fitri Lestari <549.fitrilestari@gmail.com>
2020-07-22 07:22:39 -0700

    Update resources views welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    FITRI LESTARI
+                    Fitri Lestari
                 </div>
 
                 <div class="links">
