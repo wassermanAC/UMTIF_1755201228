992d6eb5f64058b501eecca35c38071db38c586d
Fitri Lestari <549.fitrilestari@gmail.com>
2020-07-23 04:10:15 -0700

    Update public botstrap (css js)

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sistem Informasi Akademik</title>
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<script src="{{asset('js/app.js')}}"></script>
+</head>
+<body>
+	@include('layouts.navbar');
+	<h1>Test Halaman Bootstrap</h1>
+</body>
+</html>
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Halaman Mahasiswa')
+
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
